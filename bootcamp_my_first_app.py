3be34e0fb739c083b54c4b6c431e19e3e23380f7
GuillermoPrado <98121906+GuillermoPrado@users.noreply.github.com>
2022-08-13 13:40:05 -0400

    Update bootcamp_my_first_app.py

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -3,7 +3,10 @@ import streamlit as st
 st.title(" 10 Cool Beginner Python Tricks That Will Make Your Life Easier ")
 st.header(" Simple but effective tips for every python lovers ")
 
-st.markdown("<h3 style='text-align: center;'> </h3>", unsafe_allow_html=True)
+st.image(
+            "https://miro.medium.com/max/700/1*5IFgojJ4nU8f0YKTcjWDrg.jpeg",
+            width=None, caption="Photo by Miesha Maiden from Pexels",
+        )
 
 st.write(" The compactness of Python can make a developer's life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities. ")
 st.write(" In today's article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time. ")
