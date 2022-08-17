96364cf0fcf2778be53856579ee62bd81fbf770d
GuillermoPrado <98121906+GuillermoPrado@users.noreply.github.com>
2022-08-16 23:27:08 -0400

    Update bootcamp_my_second_app.py

--- bootcamp_my_second_app.py
+++ bootcamp_my_second_app.py
@@ -4,17 +4,17 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.title('CSV Reader')
-#file = st.file_uploader('upload a csv', type = "csv")
+file = st.file_uploader('upload a csv', type = "csv")
 
-#if file:
-df = pd.read_csv("train.cvs")
-st.dataframe(df)
-st.markdown("---")
+if file:
+    df = pd.read_csv("train.cvs")
+    st.dataframe(df)
+    st.markdown("---")
 
-fig1 = plt.figure(figsize = (10, 4))
-sns.countplot(x = 'Pclass', data = df)
-st.pyplot(fig1)
+    fig1 = plt.figure(figsize = (10, 4))
+    sns.countplot(x = 'Pclass', data = df)
+    st.pyplot(fig1)
 
-fig2 = plt.figure(figsize = (10, 4))
-sns.boxplot(x = 'Pclass', y = 'Age', data = df)
-st.pyplot(fig2)
+    fig2 = plt.figure(figsize = (10, 4))
+    sns.boxplot(x = 'Pclass', y = 'Age', data = df)
+    st.pyplot(fig2)
