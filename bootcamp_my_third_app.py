9ebbe5270d84e9416fc4ef9235cea0f27ca7e0e9
GuillermoPrado <98121906+GuillermoPrado@users.noreply.github.com>
2022-08-20 11:56:08 -0400

    Update bootcamp_my_third_app.py

--- bootcamp_my_third_app.py
+++ bootcamp_my_third_app.py
@@ -10,11 +10,13 @@ st.title('Stock / Inventory Discrepancy')
 
 st.markdown("---")
 
+st.write('Expected')
 df_expected = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Expected.csv", encoding="latin-1", dtype=str)
 st.dataframe(df_expected)
 
 st.markdown("---")
 
+st.write('Counted')
 df_counted = pd.read_csv("https://storage.googleapis.com/mojix-devops-wildfire-bucket/analytics/bootcamp_2_0/Bootcamp_DataAnalysis_Counted.csv", encoding="latin-1", dtype=str)
 st.dataframe(df_counted)
 
@@ -24,37 +26,31 @@ st.markdown("---")
 #df_expected.shape
 #df_expected["Retail_Product_SKU"].nunique()
 
-  # Counted Transformation
+
+
+
+
+#st.subheader('Counted Transformation')
 
 #df_counted.sample(2).T
 #df_counted.shape
 #df_counted['RFID'].nunique()
-#df_counted = df_counted.drop_duplicates("RFID")
-#df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
+df_counted = df_counted.drop_duplicates("RFID")
+st.write('Before merging the counted and expected database, its important to use ".drop_duplicates" to remove duplicates from the dataframe. In this case, its used to remove the duplicates from RFID variable in counted database.')
+df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
 #df_B.sample(10)
-#my_cols_selected = ["Retail_Product_Color",
-#"Retail_Product_Level1",
-#"Retail_Product_Level1Name",
-#"Retail_Product_Level2Name",
-#"Retail_Product_Level3Name",
-#"Retail_Product_Level4Name",
-#"Retail_Product_Name",
-#"Retail_Product_SKU",
-#"Retail_Product_Size",
-#"Retail_Product_Style",
-#"Retail_SOHQTY"]
-#df_A = df_expected[my_cols_selected]
+my_cols_selected = ["Retail_Product_Color", "Retail_Product_Level1", "Retail_Product_Level1Name", "Retail_Product_Level2Name", "Retail_Product_Level3Name", "Retail_Product_Level4Name", "Retail_Product_Name", "Retail_Product_SKU", "Retail_Product_Size", "Retail_Product_Style", "Retail_SOHQTY"]
+df_A = df_expected[my_cols_selected]
 #df_A.head().T
 
 
 
 
 
+st.write('Merged Version')
+df_discrepancy = pd.merge(df_A, df_B, how="outer", left_on="Retail_Product_SKU", right_on="Retail_Product_SKU", indicator=True)
+st.dataframe(df_discrepancy)
 
-
-
-
-#df_discrepancy = pd.merge(df_A, df_B, how="outer", left_on="Retail_Product_SKU", right_on="Retail_Product_SKU", indicator=True)
 #df_discrepancy.head()
 #df_discrepancy['Retail_CCQTY'] = df_discrepancy['Retail_CCQTY'].fillna(0)
 #df_discrepancy["Retail_CCQTY"] = df_discrepancy["Retail_CCQTY"].astype(int)
