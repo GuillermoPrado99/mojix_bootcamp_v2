8054ac91d7a9439da1f7a831de6485cb254abbfe
GuillermoPrado <98121906+GuillermoPrado@users.noreply.github.com>
2022-08-21 00:17:51 -0400

    Update bootcamp_my_third_app.py

--- bootcamp_my_third_app.py
+++ bootcamp_my_third_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.title('Stock / Inventory Discrepancy')
@@ -19,12 +18,8 @@ st.markdown("---")
 
 st.header('Plotting Expected / Counted Products')
 
-fig1 = plt.figure(figsize = (10, 4))
-sns.countplot(x = 'Retail_Product_Level1Name', data = df_expected)
-st.pyplot(fig1)
-
 fig2 = plt.figure(figsize = (10, 4))
-sns.countplot(x = 'Retail_Product_Level1Name', data = df_counted)
+ax.barh(Retail_Product_Level1Name, data = df_counted)
 st.pyplot(fig2)
 
 st.markdown("---")
