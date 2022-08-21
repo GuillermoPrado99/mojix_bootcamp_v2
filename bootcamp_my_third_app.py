e5bbef9d36f2ca0f0543440b05b7dd1e32766e4c
GuillermoPrado <98121906+GuillermoPrado@users.noreply.github.com>
2022-08-20 23:33:53 -0400

    Update bootcamp_my_third_app.py

--- bootcamp_my_third_app.py
+++ bootcamp_my_third_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 st.title('Stock / Inventory Discrepancy')
 
+st.subheader('Presenting the Stock / Inventory Databases')
+
 st.markdown("---")
 
 st.write('Expected')
@@ -26,7 +28,17 @@ st.markdown("---")
 
 #df_B = df_counted.groupby("Retail_Product_SKU").count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
 #df_B.sample(10)
-#my_cols_selected = ["Retail_Product_Color", "Retail_Product_Level1", "Retail_Product_Level1Name", "Retail_Product_Level2Name", "Retail_Product_Level3Name", "Retail_Product_Level4Name", "Retail_Product_Name", "Retail_Product_SKU", "Retail_Product_Size", "Retail_Product_Style", "Retail_SOHQTY"]
+#my_cols_selected = ["Retail_Product_Color", 
+# "Retail_Product_Level1", 
+# "Retail_Product_Level1Name", 
+# "Retail_Product_Level2Name", 
+# "Retail_Product_Level3Name", 
+# "Retail_Product_Level4Name", 
+# "Retail_Product_Name", 
+# "Retail_Product_SKU", 
+# "Retail_Product_Size", 
+# "Retail_Product_Style", 
+# "Retail_SOHQTY"]
 #df_A = df_expected[my_cols_selected]
 #df_A.head().T
 
